5aaad518c0fac65e0fd65bc1b2a37e7dfc5ae2e9
Abhijeet <abhijeetpoudel@gmail.com>
2023-08-29 20:43:49 +0545

    feat: finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,39 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable react/prop-types */
-
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
-import { Game } from "../src/Game.js";
-import { State } from "../src/Game.js";
+import { Game } from "../src/game/Game.js";
+import { State } from "../src/game/Game.js";
 import { parse, stringify } from "flatted";
 import { searchResults } from "../src/Search.js";
 import Node from "./Node.jsx";
-import "./App.css";
-import { GameState } from "./config.js";
-import StateLevel from "./StateLevel.jsx";
+import "./styles/App.css";
+import { GameState } from "./game/config.js";
 
 let data = parse(stringify(searchResults));
 console.log(data);
-let node = data.parent;
-let startState = new State(3, 3, false);
-while (node.parent != null) {
-  node = node.parent;
-}
-console.log(node);
 
 const useDataParser = (d) => {
-  const [data, setData] = useState({
-    gameStatus: d.data.gameStatus,
-    movePerformed: d.data.movePerformed
-      ? `Missionaries:${d.data.movePerformed.missionaries}, Cannibals:${d.data.movePerformed.cannibals}`
-      : "No move performed",
-    state: d.data.state
-      ? `Missionaries:${d.data.state.missionaries},Cannibals:${d.data.state.cannibals},Boat:${d.data.state.boat}`
-      : "No state",
-  });
+	const [data, setData] = useState({
+		gameStatus: d.data.gameStatus,
+		movePerformed: d.data.movePerformed
+			? `Missionaries:${d.data.movePerformed.missionaries}, Cannibals:${d.data.movePerformed.cannibals}`
+			: "No move performed",
+		state: d.data.state
+			? `Missionaries:${d.data.state.missionaries},Cannibals:${d.data.state.cannibals},Boat:${d.data.state.boat}`
+			: "No state",
+	});
 
-  // console.log(d);
-  return data;
+	// console.log(d);
+	return data;
 };
-
-const TreeNode = ({ node, depth }) => {
-  return (
-    <div className="tree-node">
-      <StateLevel node={node} />
-
-      {/* {node.children.map((child, idx) => {
-        console.log("sent");
-        <StateLevel />;
-      })} */}
-    </div>
-    // <div className="tree-node">
-    //   {node.children.map((child, idx) => {
-
-    //     const childData = useDataParser(child);
-    //     return (
-    //       <div key={idx}>
-    //         <Node childData={childData} />
-
-    //         {childData.gameStatus == "running" && child.children.length > 0
-    //           ? child.children.map((childchild, idd) => {
-    //               console.log(childchild);
-    //               const childDataData = useDataParser(childchild);
-    //               return (
-    //                 <div key={idd}>
-    //                   <Node childData={childDataData} />
-
-    //                   {childDataData.gameStatus == "running" &&
-    //                   childchild.children.length > 0
-    //                     ? child.children.map((childchildchild, iddd) => {
-    //                         const childDataDataData =
-    //                           useDataParser(childchildchild);
-    //                         return (
-    //                           <div key={iddd}>
-    //                             <Node childData={childDataDataData} />
-    //                           </div>
-    //                         );
-    //                       })
-    //                     : "No children"}
-    //                 </div>
-    //               );
-    //             })
-    //           : "No more children"}
-    //       </div>
-    //     );
-    //   })}
-    // </div>
-  );
-};
-
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Game Search Tree</h1>
-      <TreeNode node={node} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Missionaries-Cannibal Game Search Tree</h1>
+			<Node node={data} />
+		</div>
+	);
 };
 
 export default App;
--- src/Node.jsx
+++ src/Node.jsx
@@ -1,321 +1,343 @@
 /* eslint-disable react/prop-types */
 import { ArcherContainer, ArcherElement } from "react-archer";
-import "./node.css";
+import "./styles/node.css";
 
 const Node = (props) => {
   return (
-    <div className="div_main">
-      <ArcherContainer strokeColor="black">
-        <ArcherElement
-          id="root"
-          relations={[
-            {
-              targetId: "element1",
-              targetAnchor: "bottom",
-              sourceAnchor: "top",
-              style: { strokeColor: "red", strokeWidth: 1 },
-            },
-            {
-              targetId: "element2",
-              targetAnchor: "bottom",
-              sourceAnchor: "top",
-              style: { strokeColor: "red", strokeWidth: 1 },
-            },
-            {
-              targetId: "element3",
-              targetAnchor: "bottom",
-              sourceAnchor: "top",
-              style: { strokeColor: "red", strokeWidth: 1 },
-            },
-            {
-              targetId: "element4",
-              targetAnchor: "bottom",
-              sourceAnchor: "top",
-              style: { strokeColor: "red", strokeWidth: 1 },
-            },
-            {
-              targetId: "element5",
-              targetAnchor: "bottom",
-              sourceAnchor: "top",
-              style: { strokeColor: "red", strokeWidth: 1 },
-            },
-          ]}
-        >
-          <div className="node">(3,3,0)</div>
-        </ArcherElement>
-        <div className="baksa">
-          <ArcherElement id="element1" relations={[{}]}>
-            <div className="node_end">(2,3,1)</div>
-          </ArcherElement>
-          <ArcherElement id="element2" relations={[{}]}>
-            <div className="node_end">(3,2,1)</div>
-          </ArcherElement>
-          <ArcherElement id="element3" relations={[{}]}>
-            <div className="node_end">(1,3,1)</div>
-          </ArcherElement>
-          <ArcherElement
-            id="element4"
-            relations={[
-              {
-                targetId: "element6",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "red", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(3,1,1)</div>
-          </ArcherElement>
-          <ArcherElement
-            id="element5"
-            relations={[
-              {
-                targetId: "element6",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element7",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(2,2,1)</div>
-          </ArcherElement>
-        </div>
-        <div className="baksa">
-          <ArcherElement
-            id="element6"
-            relations={[
-              {
-                targetId: "element8",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element9",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element10",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(3,2,0)</div>
-          </ArcherElement>
+		<div className="div_main">
+			<ArcherContainer strokeColor="black" noCurves={false} offset={2}>
+				<div className="baksa">
+					<ArcherElement
+						id="start"
+						relations={[
+							{
+								targetId: "element1",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(1,0)",
+							},
+							{
+								targetId: "element2",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,1)",
+							},
+							{
+								targetId: "element3",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(2,0)",
+							},
+							{
+								targetId: "element4",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,2)",
+							},
+							{
+								targetId: "element5",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(1,1)",
+							},
+						]}
+					>
+						<div className="node">(3,3,0)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement id="element1" relations={[{}]}>
+						<div className="node dead_node">(2,3,1)</div>
+					</ArcherElement>
+					<ArcherElement id="element2" relations={[{}]}>
+						<div className="node">(3,2,1)</div>
+					</ArcherElement>
+					<ArcherElement id="element3" relations={[{}]}>
+						<div className="node dead_node">(1,3,1)</div>
+					</ArcherElement>
+					<ArcherElement
+						id="element4"
+						relations={[
+							{
+								targetId: "element6",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,1)",
+							},
+						]}
+					>
+						<div className="node">(3,1,1)</div>
+					</ArcherElement>
+					<ArcherElement
+						id="element5"
+						relations={[
+							{
+								targetId: "element7",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,1)",
+							},
+						]}
+					>
+						<div className="node">(2,2,1)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement
+						id="element6"
+						relations={[
+							{
+								targetId: "element8",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(2,0)",
+							},
+							{
+								targetId: "element9",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,2)",
+							},
+							{
+								targetId: "element10",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(1,1)",
+							},
+						]}
+					>
+						<div className="node">(3,2,0)</div>
+					</ArcherElement>
 
-          <ArcherElement id="element7" relations={[{}]}>
-            <div className="node_end">(2,3,0)</div>
-          </ArcherElement>
-        </div>
-        <div className="baksa">
-          <ArcherElement id="element8" relations={[{}]}>
-            <div className="node_end">(1,2,1)</div>
-          </ArcherElement>
+					<ArcherElement id="element7" relations={[{}]}>
+						<div className="node dead_node">(2,3,0)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement id="element8" relations={[{}]}>
+						<div className="node dead_node">(1,2,1)</div>
+					</ArcherElement>
 
-          <ArcherElement
-            id="element9"
-            relations={[
-              {
-                targetId: "element11",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(3,0,1)</div>
-          </ArcherElement>
-          <ArcherElement id="element10" relations={[{}]}>
-            <div className="node_end">(2,1,1)</div>
-          </ArcherElement>
-        </div>
-        <div className="baksa">
-          <ArcherElement
-            id="element11"
-            relations={[
-              {
-                targetId: "element12",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element13",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(3, 1, 0)</div>
-          </ArcherElement>
-        </div>
-        <div className="baksa">
-          <ArcherElement
-            id="element12"
-            relations={[
-              {
-                targetId: "element14",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element15",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element16",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element17",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(1,1,1)</div>
-          </ArcherElement>
-          <ArcherElement id="element13" relations={[{}]}>
-            <div className="node_end">(2,0,1)</div>
-          </ArcherElement>
-        </div>
-        <div className="baksa">
-          <ArcherElement id="element14" relations={[{}]}>
-            <div className="node_end">(2,1,0)</div>
-          </ArcherElement>
-          <ArcherElement id="element15" relations={[{}]}>
-            <div className="node_end">(1,2,0)</div>
-          </ArcherElement>
-          <ArcherElement id="element16" relations={[{}]}>
-            <div className="node_end">(1,3,0)</div>
-          </ArcherElement>
-          <ArcherElement
-            id="element17"
-            relations={[
-              {
-                targetId: "element18",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(2,2,0)</div>
-          </ArcherElement>
-        </div>
-        <div className="baksa">
-          <ArcherElement
-            id="element18"
-            relations={[
-              {
-                targetId: "element19",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(0,2,1)</div>
-          </ArcherElement>
-        </div>
-        <div className="baksa">
-          <ArcherElement
-            id="element19"
-            relations={[
-              {
-                targetId: "element20",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(0,3,0)</div>
-          </ArcherElement>
-        </div>
+					<ArcherElement
+						id="element9"
+						relations={[
+							{
+								targetId: "element11",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,1)",
+							},
+						]}
+					>
+						<div className="node">(3,0,1)</div>
+					</ArcherElement>
+					<ArcherElement id="element10" relations={[{}]}>
+						<div className="node dead_node">(2,1,1)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement
+						id="element11"
+						relations={[
+							{
+								targetId: "element12",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(2,0)",
+							},
+							{
+								targetId: "element13",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(1,1)",
+							},
+						]}
+					>
+						<div className="node">(3, 1, 0)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement
+						id="element12"
+						relations={[
+							{
+								targetId: "element14",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(1,0)",
+							},
+							{
+								targetId: "element15",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,1)",
+							},
+							{
+								targetId: "element16",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,2)",
+							},
+							{
+								targetId: "element17",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(1,1)",
+							},
+						]}
+					>
+						<div className="node">(1,1,1)</div>
+					</ArcherElement>
+					<ArcherElement id="element13" relations={[{}]}>
+						<div className="node dead_node">(2,0,1)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement id="element14" relations={[{}]}>
+						<div className="node dead_node">(2,1,0)</div>
+					</ArcherElement>
+					<ArcherElement id="element15" relations={[{}]}>
+						<div className="node dead_node">(1,2,0)</div>
+					</ArcherElement>
+					<ArcherElement id="element16" relations={[{}]}>
+						<div className="node dead_node">(1,3,0)</div>
+					</ArcherElement>
+					<ArcherElement
+						id="element17"
+						relations={[
+							{
+								targetId: "element18",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(2,0)",
+							},
+						]}
+					>
+						<div className="node">(2,2,0)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement
+						id="element18"
+						relations={[
+							{
+								targetId: "element19",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,1)",
+							},
+						]}
+					>
+						<div className="node">(0,2,1)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement
+						id="element19"
+						relations={[
+							{
+								targetId: "element20",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,2)",
+							},
+						]}
+					>
+						<div className="node">(0,3,0)</div>
+					</ArcherElement>
+				</div>
 
-        <div className="baksa">
-          <ArcherElement
-            id="element20"
-            relations={[
-              {
-                targetId: "element21",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element22",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(0,1,1)</div>
-          </ArcherElement>
-        </div>
-        <div className="baksa">
-          <ArcherElement
-            id="element21"
-            relations={[
-              {
-                targetId: "element23",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-              {
-                targetId: "element24",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(1,1,0)</div>
-          </ArcherElement>
-          <ArcherElement
-            id="element22"
-            relations={[
-              {
-                targetId: "element24",
-                targetAnchor: "bottom",
-                sourceAnchor: "top",
-                style: { strokeColor: "lime", strokeWidth: 1 },
-              },
-            ]}
-          >
-            <div className="node">(0,2,0)</div>
-          </ArcherElement>
-        </div>
-
-        <ArcherElement id="element23" relations={[{}]}>
-          <div className="node_end">(1,0,1)</div>
-        </ArcherElement>
-        <ArcherElement id="element24" relations={[{}]}>
-          <div className="node">(0,0,1)</div>
-        </ArcherElement>
-      </ArcherContainer>
-    </div>
-  );
+				<div className="baksa">
+					<ArcherElement
+						id="element20"
+						relations={[
+							{
+								targetId: "element21",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(1,0)",
+							},
+							{
+								targetId: "element22",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,1)",
+							},
+						]}
+					>
+						<div className="node">(0,1,1)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement
+						id="element21"
+						relations={[
+							{
+								targetId: "element23",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,1)",
+							},
+							{
+								targetId: "element24",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(1,1)",
+							},
+						]}
+					>
+						<div className="node">(1,1,0)</div>
+					</ArcherElement>
+					<ArcherElement
+						id="element22"
+						relations={[
+							{
+								targetId: "element24",
+								targetAnchor: "top",
+								sourceAnchor: "bottom",
+								style: { strokeColor: "gray", strokeWidth: 1 },
+								label: "(0,2)",
+							},
+						]}
+					>
+						<div className="node">(0,2,0)</div>
+					</ArcherElement>
+				</div>
+				<div className="baksa">
+					<ArcherElement id="element23" relations={[{}]}>
+						<div className="node dead_node">(1,0,1)</div>
+					</ArcherElement>
+					<ArcherElement id="element24" relations={[{}]}>
+						<div className="node won_node">(0,0,1)</div>
+					</ArcherElement>
+				</div>
+			</ArcherContainer>
+		</div>
+	);
 };
 
 export default Node;
